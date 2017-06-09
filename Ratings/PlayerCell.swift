7e53d3bf2a6f7a83566a66cc82b634629a1b2d41
@iOSRunner <@ericguo1213@163.com>
2017-06-09 16:58:40 +0800

    使用自定义的cell子类

---
+++ Ratings/PlayerCell.swift
@@ -0,0 +1,45 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Wanlei on 2017/6/9.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+    
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    var player: Player! {
+        didSet {
+            gameLabel.text = player.game
+            nameLabel.text = player.name
+            ratingImageView.image = imageForRating(rating: player.rating)
+        }
+    }
+    
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+
+    
+
+}
--- Ratings/PlayersViewController.swift
+++ Ratings/PlayersViewController.swift
@@ -42,29 +42,19 @@ class PlayersViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath) as! PlayerCell
 
         // Configure the cell...
 
         let player = players[indexPath.row] as Player
         
-        if let nameLabel = cell.viewWithTag(100) as? UILabel { //3
-            nameLabel.text = player.name
-        }
-        if let gameLabel = cell.viewWithTag(101) as? UILabel {
-            gameLabel.text = player.game
-        }
-        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
-            ratingImageView.image = self.imageForRating(rating: player.rating)
-        }
+        cell.player = player
+        
         
         return cell
+        
     }
     
-    func imageForRating(rating:Int) -> UIImage? {
-        let imageName = "\(rating)Stars"
-        return UIImage(named: imageName)
-    }
     
 
     /*
